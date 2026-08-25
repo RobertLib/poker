250540987e0963b6f9f4a23d870e42310255679a
RobertLib <rob@hotmail.cz>
2026-08-25 18:34:06 +0200

    Initial Commit

---
+++ poker/pokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokerApp.swift
+//  poker
+//
+//  Created by Robert Libšanský on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct pokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
